45fd87e122a4402081413cd479e8a293239d94ff
psyrgkan <psyrganis@gmail.com>
2022-10-14 14:48:36 +0200

    added sample pokemon

--- pokemon.py
+++ pokemon.py
@@ -8,12 +8,14 @@ class Pokemon:
 	'''
 	Class takes in:
 
-	stats: in list format where stats[0] is HP, stats[1] is Attack, stats[2] is Defence,
+	stats: list[int]: in list format where stats[0] is HP, stats[1] is Attack, stats[2] is Defence,
 			stats[3] is Special Attack, stats[4] is Special Defence and stats[5] is Speed
 
-	type: gives the type of the pokemon in str format ('water', 'fire', 'flying', etc.)
+	type: str: gives the type of the pokemon in str format ('water', 'fire', 'flying', etc.)
+
+	attacks: list[Attack]: gives the list of all possible attacks a pokemon can learn
 	'''
-	def __init__(self, stats: list, tipo: str, attacks: list[Attack]):
+	def __init__(self, stats: list, tipo: str, attacks: list[Attack] = []):
 		self.stats = stats
 		self.tipo = tipo
 		self.hp = stats[0]
