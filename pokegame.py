2bd9478efe53b320f9796e0e189f62a486bfd2b3
psyrgkan <psyrganis@gmail.com>
2022-10-14 13:56:12 +0200

    imported

--- pokegame.py
+++ pokegame.py
@@ -2,6 +2,7 @@
 This is the main python file where the logic and practice for the fight simulator lives.
 
 '''
-import Attack from attack
-import Pokemon from pokemon
+from attack import Attack 
+from pokemon import Pokemon 
+
 
